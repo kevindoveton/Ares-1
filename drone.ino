91910caa5fb6f82ed7db16c3e3dd33348c8442c8
kevindoveton <kevindoveton344@gmail.com>
2016-06-04 12:05:22 +0930

    project deemed a failure..

--- drone.ino
+++ drone.ino
@@ -3,26 +3,33 @@
 // 2015 - 2016, Kevin Doveton, Lewis Daly
 
 /* --------  CONFIG  --------*/
+#define ROLLRATE 180
+#define PITCHRATE 180
+#define YAWRATE 90
+
 #define ACROMODE 1 // set to 1 for acro mode, 0 for stabilise
+// #define TESTMODE
 
-#define stabPitchP	2.5
-#define stabPitchI	0
-#define stabPitchD	0
-#define stabRollP	2.5
-#define stabRollI	0
-#define stabRollD	0
-#define stabYawP	2.5
-#define stabYawI	0
-#define stabYawD	0
+#if ACROMODE == 0
+	#define stabPitchP	2.5
+	#define stabPitchI	0
+	#define stabPitchD	0
+	#define stabRollP	2.5
+	#define stabRollI	0
+	#define stabRollD	0
+	#define stabYawP	2.5
+	#define stabYawI	0
+	#define stabYawD	0
+#endif
 
-#define ratePitchP	0.35
+#define ratePitchP	0.8
 #define ratePitchI	0
 #define ratePitchD	0
-#define rateRollP	0.35
+#define rateRollP	0.8
 #define rateRollI	0
 #define rateRollD	0
-#define rateYawP	0.2
-#define rateYawI	0
+#define rateYawP	0.1
+#define rateYawI	0.05
 #define rateYawD	0
 
 #include "receiver.h"
@@ -40,18 +47,20 @@ int ch1, ch2, ch3, ch4;
 
 // pids
 double pitch_rate_output, roll_rate_output, yaw_rate_output;
-double pitch_stab_output, roll_stab_output, yaw_stab_output;
 double pitch_rate_input, roll_rate_input, yaw_rate_input;
-double pitch_stab_input, roll_stab_input, yaw_stab_input;
 double pitch_rate_error, roll_rate_error, yaw_rate_error;
-double pitch_stab_error, roll_stab_error, yaw_stab_error;
-
 PID PID_PITCH_RATE(&pitch_rate_input, &pitch_rate_output, &pitch_rate_error, ratePitchP, ratePitchI, ratePitchD, REVERSE);
 PID PID_ROLL_RATE(&roll_rate_input, &roll_rate_output, &roll_rate_error, rateRollP, rateRollI, rateRollD, REVERSE);
 PID PID_YAW_RATE(&yaw_rate_input, &yaw_rate_output, &yaw_rate_error, rateYawP, rateYawI, rateYawD, REVERSE);
-PID PID_PITCH_STAB(&pitch_stab_input, &pitch_stab_output, &pitch_stab_error, stabPitchP, stabPitchI, stabPitchD, DIRECT);
-PID PID_ROLL_STAB(&roll_stab_input, &roll_stab_output, &roll_stab_error, stabRollP, stabRollI, stabRollD, DIRECT);
-PID PID_YAW_STAB(&yaw_stab_input, &yaw_stab_output, &yaw_stab_error, stabYawP, stabYawI, stabYawD, DIRECT);
+
+#if ACROMODE == 0
+	double pitch_stab_output, roll_stab_output, yaw_stab_output;
+	double pitch_stab_input, roll_stab_input, yaw_stab_input;
+	double pitch_stab_error, roll_stab_error, yaw_stab_error;
+	PID PID_PITCH_STAB(&pitch_stab_input, &pitch_stab_output, &pitch_stab_error, stabPitchP, stabPitchI, stabPitchD, DIRECT);
+	PID PID_ROLL_STAB(&roll_stab_input, &roll_stab_output, &roll_stab_error, stabRollP, stabRollI, stabRollD, DIRECT);
+	PID PID_YAW_STAB(&yaw_stab_input, &yaw_stab_output, &yaw_stab_error, stabYawP, stabYawI, stabYawD, DIRECT);
+#endif
 
 #define wrap_180(x) (x < -180 ? x+360 : (x > 180 ? x - 360: x))
 // Wrap 180 - Contain results within -180 and +180
@@ -63,7 +72,9 @@ void setup()
 {
 	// This must be first
 	// Start Serial Monitor
-	Serial.begin(9600);
+	#ifdef TESTMODE
+		Serial.begin(9600);
+	#endif
 	// Serial.println("started serial");
 
 	// // Initiate Receiver
@@ -75,24 +86,28 @@ void setup()
 	PID_PITCH_RATE.SetMode(AUTOMATIC);
 	PID_ROLL_RATE.SetMode(AUTOMATIC);
 	PID_YAW_RATE.SetMode(AUTOMATIC);
-	PID_PITCH_STAB.SetMode(AUTOMATIC);
-	PID_ROLL_STAB.SetMode(AUTOMATIC);
-	PID_YAW_STAB.SetMode(AUTOMATIC);
-
 	// Set PID limits
-	PID_PITCH_STAB.SetOutputLimits(-250, 250);
-	PID_ROLL_STAB.SetOutputLimits(-250, 250);
-	PID_YAW_STAB.SetOutputLimits(-360, 360);
 	PID_PITCH_RATE.SetOutputLimits(-500, 500);
 	PID_ROLL_RATE.SetOutputLimits(-500, 500);
 	PID_YAW_RATE.SetOutputLimits(-500, 500);
 
-	PID_PITCH_STAB.SetSampleTime(1);
-	PID_ROLL_STAB.SetSampleTime(1);
-	PID_YAW_STAB.SetSampleTime(1);
 	PID_PITCH_RATE.SetSampleTime(1);
 	PID_ROLL_RATE.SetSampleTime(1);
 	PID_YAW_RATE.SetSampleTime(1);
+
+	#if ACROMODE == 0
+		PID_PITCH_STAB.SetMode(AUTOMATIC);
+		PID_ROLL_STAB.SetMode(AUTOMATIC);
+		PID_YAW_STAB.SetMode(AUTOMATIC);
+
+		PID_PITCH_STAB.SetOutputLimits(-250, 250);
+		PID_ROLL_STAB.SetOutputLimits(-250, 250);
+		PID_YAW_STAB.SetOutputLimits(-360, 360);
+
+		PID_PITCH_STAB.SetSampleTime(1);
+		PID_ROLL_STAB.SetSampleTime(1);
+		PID_YAW_STAB.SetSampleTime(1);
+	#endif
 
 	// read high
 	while(!motors.motorsArmed())
@@ -177,9 +192,16 @@ void loop()
 
 	long rcthr, rcyaw, rcpit, rcroll;   // Variables to store rc input
 	rcthr = ch3;
-	rcyaw = map(ch1, receiver.minYaw, receiver.maxYaw, -150, 150);
-	rcpit = map(ch2, receiver.minPitch, receiver.maxPitch, 45, -45);
-	rcroll = map(ch4, receiver.minRoll, receiver.maxRoll, -45, 45);
+	#if ARCOMODE == 0 // stable mode, these are the max rolls
+		rcyaw = map(ch1, receiver.minYaw, receiver.maxYaw, -150, 150);
+		rcpit = map(ch2, receiver.minPitch, receiver.maxPitch, 45, -45);
+		rcroll = map(ch4, receiver.minRoll, receiver.maxRoll, -45, 45);
+	#endif
+	#if ACROMODE == 1 // acro mode, rates
+		rcyaw = map(ch1, receiver.minYaw, receiver.maxYaw, -YAWRATE, YAWRATE);
+		rcpit = map(ch2, receiver.minPitch, receiver.maxPitch, PITCHRATE, -PITCHRATE);
+		rcroll = map(ch4, receiver.minRoll, receiver.maxRoll, -ROLLRATE, ROLLRATE);
+	#endif
 	// Serial.println("Thr: " + String(rcthr) + " Yaw: " + String(rcyaw) + " Pit: " + String(rcpit) + " Roll: " + String(rcroll));
 
 
@@ -188,11 +210,13 @@ void loop()
 
 	// down + right +
 	sensors.updateReadings();
-	EulerAngle sensorVector = sensors.getEuler();
-	float roll = ToDeg(sensorVector.roll);
-	float pitch = ToDeg(sensorVector.pitch);
-	float yaw = ToDeg(sensorVector.yaw);
-	// Serial.println(String(roll) + " " + String(pitch) + " " + String(yaw));
+	#if ACROMODE == 0
+		EulerAngle sensorVector = sensors.getEuler();
+		float roll = ToDeg(sensorVector.roll);
+		float pitch = ToDeg(sensorVector.pitch);
+		float yaw = ToDeg(sensorVector.yaw);
+		// Serial.println(String(roll) + " " + String(pitch) + " " + String(yaw));
+	#endif
 
 	EulerAngle gyroVector = sensors.getGyroEuler();
 	float gyroRoll = gyroVector.roll;
@@ -261,7 +285,7 @@ void loop()
 			// end, find the ratio of current motor to maxMotor and multiply by error.
 			// the highest motor will equal the maxThrottle value and the others will be
 			// proportional to their original value.
-			float error = 0;
+			/*float error = 0;
 			float maxMotor = 0;
 
 			if (FL > receiver.maxThrottle)
@@ -303,10 +327,10 @@ void loop()
 				BL -= ((BL/maxMotor) * error);
 				FR -= ((FR/maxMotor) * error);
 				BR -= ((BR/maxMotor) * error);
-			}
-
-			motors.setSpeeds(FL, FR, BR, BL); // Set Motor Speeds
-
+			}*/
+			#ifndef TESTMODE
+				motors.setSpeeds(FL, FR, BR, BL); // Set Motor Speeds
+			#endif
 
 			// TESTING
 			// -----------------------------------------
@@ -325,8 +349,10 @@ void loop()
 		{
 			motors.setSpeeds(receiver.minThrottle,receiver.minThrottle,receiver.minThrottle,receiver.minThrottle);
 
-			// reset yaw target so we maintain this on takeoff
-			yaw_target = yaw;
+			#if ACROMODE == 0
+				// reset yaw target so we maintain this on takeoff
+				yaw_target = yaw;
+			#endif
 
 			// reset PID integrals whilst on the ground
 		}
@@ -336,7 +362,9 @@ void loop()
 		// MOTORS OFF
 		motors.setSpeeds(receiver.minThrottle,receiver.minThrottle,receiver.minThrottle,receiver.minThrottle);
 
+		#if ACROMODE == 0
 			// reset yaw target so we maintain this on takeoff
 			yaw_target = yaw;
+		#endif
 	}
 }
